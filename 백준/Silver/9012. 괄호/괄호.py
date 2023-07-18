92c7603ac64f47297d73e3eac6b8a86b8daf3cab
yesjm <48047620+yesjm78@users.noreply.github.com>
2023-07-18 22:31:17 +0900

    python 문법 반영하여 수정
    for ~ else 문
    List가 empty인지 확인하는 방법

--- 백준/Silver/9012. 괄호/괄호.py
+++ 백준/Silver/9012. 괄호/괄호.py
@@ -1,19 +1,18 @@
-n = int(input())
-stack = []
-for _ in range(n):
+t = int(input())
+for _ in range(t):
+    stack = []
     ps = str(input())
-    vps = True
     for i in ps:
         if i == '(':
             stack.append(i)
-        else:
-            if len(stack) > 0:
+        elif i == ')':
+            if stack:
                 stack.pop()
             else:
-                vps = False
+                print('NO')
                 break
-    if len(stack) != 0 or vps == False:
-        print('NO')
-        stack.clear()
-    else:
-        print('YES')
+    else: # break문으로 끊기지 않고 수행됬을경우 수행한다
+        if not stack:
+            print('YES')
+        else:
+            print('NO')
