94473fe2e608c26ea4572b37328fd43bbc69dec8
yesjm <48047620+yesjm-dev@users.noreply.github.com>
2023-09-15 23:54:05 +0900

    [Silver III] Title: 1로 만들기, Time: 560 ms, Memory: 39068 KB -BaekjoonHub

---
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.py
@@ -0,0 +1,9 @@
+n = int(input())
+cache = [0] * (n+1)
+for i in range(2, n+1):
+    cache[i] = cache[i-1]+1
+    if i%2 == 0:
+        cache[i] = min(cache[i], cache[i//2]+1)
+    if i%3 == 0:
+        cache[i] = min(cache[i], cache[i//3]+1)
+print(cache[n])
