85c912bb86314f81d5e845bc93de8a1d9a4f7c83
노정민 <48047620+yesjm-dev@users.noreply.github.com>
2023-09-25 22:59:35 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 48 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.py
@@ -0,0 +1,10 @@
+import sys
+sys.setrecursionlimit(10**6)
+input = sys.stdin.readline
+n = int(input())
+dp = [0] * 1001
+dp[1] = 1
+dp[2] = 3
+for i in range(3, n+1):
+    dp[i] = (dp[i-1] + 2*dp[i-2]) % 10007
+print(dp[n])
