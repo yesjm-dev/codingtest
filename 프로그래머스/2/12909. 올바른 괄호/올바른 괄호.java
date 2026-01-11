4fb846bd5e6dfe54daeca38559d9a175d1de0010
노정민 <48047620+yesjm-dev@users.noreply.github.com>
2026-01-11 20:34:42 +0900

    [level 2] Title: 올바른 괄호, Time: 6.17 ms, Memory: 54.2 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -1,14 +1,15 @@
 class Solution {
 
     boolean solution(String s) {
-        if (s.charAt(0) == ')' || s.charAt(s.length() - 1) == '(')
-            return false;
-        
         int count = 0;
         for (char c : s.toCharArray()) {
-            if (c == '(') count++;
-            else if (c == ')') count--;
-            if (count < 0) return false;
+            if (c == '(')
+                count++;
+            else if (c == ')')
+                count--;
+
+            if (count < 0)
+                return false;
         }
 
         return count == 0;
